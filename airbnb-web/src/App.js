23a0fbb454ecd6a2c51c71137a5c785d26253366
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-30 15:12:17 -0300

    Add services and routes

--- airbnb-web/src/App.js
+++ airbnb-web/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
-}
+import Routes from './routes';
+
+const App = () => <Routes />;
 
 export default App;
